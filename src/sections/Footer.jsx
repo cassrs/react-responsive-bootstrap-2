39196bceda9efd50f0cc595da957e6103d05f93b
palaarda <gametime.plz@outlook.com>
2024-01-05 00:49:27 +0300

    footer-update

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer className="py-5 bg-dark">
-            <div className="container"><p className="m-0 text-center text-white">Copyright © Arda Pala 2023</p></div>
+            <div className="container"><p className="m-0 text-center text-white">Copyright © <a href="https://github.com/palaarda" target="_blank">Arda Pala</a> 2023</p></div>
         </footer>
     )
 }
