6caf111e15e8e665392d5bb1bd9053809c5dc643
palaarda <gametime.plz@outlook.com>
2024-01-04 23:52:41 +0300

    change

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -2,7 +2,7 @@ const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container px-lg-5">
-                <a className="navbar-brand" href="#!">Start Bootstrap</a>
+                <a className="navbar-brand" href="#!">Close AI</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
