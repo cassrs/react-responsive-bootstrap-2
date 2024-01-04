cedd3ec175c255828c04564ae33f2be720fb4211
palaarda <gametime.plz@outlook.com>
2024-01-05 00:18:48 +0300

    theme

--- src/sections/Features.jsx
+++ src/sections/Features.jsx
@@ -5,12 +5,12 @@ const Features = () => {
         <section className="pt-4">
             <div className="container px-lg-5">
                 <div className="row gx-lg-5">
-                    <Card iconName="collection" title="Fresh new layout" text="With Bootstrap 5, we've created a fresh new layout for this template!" />
-                    <Card iconName="cloud-download" title="Free to download" text="As always, Start Bootstrap has a powerful collectin of free templates." />
-                    <Card iconName="card-heading" title="Jumbotron hero header" text="The heroic part of this template is the jumbotron hero header!" />
-                    <Card iconName="bootstrap" title="Feature boxes" text="We've created some custom feature boxes using Bootstrap icons!" />
-                    <Card iconName="code" title="Simple clean code" text="We keep our dependencies up to date and squash bugs as they come!" />
-                    <Card iconName="patch-check" title="A name you trust" text="Start Bootstrap has been the leader in free Bootstrap templates since 2013!" />
+                    <Card iconName="mic" title="Sesli Komutlar" text="TPG AI, sesli komutlarınızı anlayarak size hızlı ve doğru cevaplar verir. İhtiyaçlarınıza uygun bilgiyi ve yönergeleri sunar." />
+                    <Card iconName="headset" title="Kişisel Asistan" text="TPG AI, takvim yönetimi, hatırlatıcılar, notlar ve daha fazlası gibi kişisel görevlerinizi kolayca yönetmenize yardımcı olur. Her zaman yanınızda olan bir asistan gibi çalışır." />
+                    <Card iconName="archive-fill" title="Bilgiye Erişim" text="TPG AI, geniş bir bilgi tabanına sahiptir ve sorularınıza anında cevaplar verir. Araştırma yaparken size önemli bilgiler sunar." />
+                    <Card iconName="box-fill" title="Özelleştirilebilir" text="TPG AI, sizin tercihlerinize ve ihtiyaçlarınıza göre özelleştirilebilir. Size en uygun deneyimi sağlar." />
+                    <Card iconName="shield-lock-fill" title="Güvenlik" text="TPG AI, güçlü güvenlik önlemleri ile korunur. Kişisel verilerinizin gizliliğini korur ve güvenli bir şekilde kullanır." />
+                    <Card iconName="translate" title="Dil Çevirisi ve İletişim Kolaylığı" text="TPG AI, farklı diller arasında hızlı ve doğru çeviriler yapabilir, Uluslararası işlerde dil engellerini aşmanıza destek olur." />
                 </div>
             </div>
         </section>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,8 +5,8 @@ const Hero = () => {
                 <div className="p-4 p-lg-5 bg-light rounded-3 text-center">
                     <div className="m-4 m-lg-5">
                         <h1 className="display-5 fw-bold">Close AI, Chat TPG 4.0</h1>
-                        <p className="fs-4">Bootstrap utility classes are used to create this jumbotron since the old component has been removed from the framework. Why create custom CSS when you can use utilities?</p>
-                        <a className="btn btn-primary btn-lg" href="#!">Call to action</a>
+                        <p className="fs-4">TPG, güçlü bir yapay zeka tabanlı yardımcıdır, her türlü görevi basit ve etkili bir şekilde yerine getirmeniz için size destek sağlar. TPG, kullanıcıların yaşamlarını daha kolay ve verimli hale getirmek için tasarlanmıştır.</p>
+                        <a className="btn btn-primary btn-lg" href="#!">Hemen Başla!</a>
                     </div>
                 </div>
             </div>
