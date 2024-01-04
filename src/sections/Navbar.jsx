45d32b19ef0cb1132c9875a9f620dce182c5e66f
palaarda <gametime.plz@outlook.com>
2024-01-05 00:19:23 +0300

    nav-change-(theme)

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -6,9 +6,9 @@ const Navbar = () => {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li className="nav-item"><a className="nav-link active" aria-current="page" href="#!">Home</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#!">About</a></li>
-                        <li className="nav-item"><a className="nav-link" href="#!">Contact</a></li>
+                        <li className="nav-item"><a className="nav-link active" aria-current="page" href="#!">Anasayfa</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#!">Hakkımızda</a></li>
+                        <li className="nav-item"><a className="nav-link" href="#!">İletişim</a></li>
                     </ul>
                 </div>
             </div>
